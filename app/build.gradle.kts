0c1518d4b2bdb34034791fea091e917bb5e0e8f4
h8masha <smirnovkosta95@gmail.com>
2026-09-15 15:38:41 +0300

    Was added linter (formatter) and configured java project to JDK21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,21 @@
 plugins {
     id("java")
     application
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+spotless {
+    java {
+        // Сортировка импортов
+        importOrder()
+        // Удаление неиспользуемых импортов
+        removeUnusedImports()
+        // Форматирование по стандарту Google (AOSP variant - Android Open Source Project, часто используется как более строгий вариант)
+        googleJavaFormat().aosp()
+        // Исправление аннотаций
+        formatAnnotations()
+        // Замена табуляции на 4 пробела (на всякий случай, хотя google-java-format обычно сам справляется)
+    }
 }
 
 application {
